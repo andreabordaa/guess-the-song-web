2a5b6a0960ae4a3c3705ab8b2f1da287bb4eb675
Andrea Borda <aborda2@uncc.edu>
2026-05-15 21:13:20 -0400

    fix: playlist image view

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "mosaic.scdn.co",
+      },
+      {
+        protocol: "https",
+        hostname: "i.scdn.co",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
